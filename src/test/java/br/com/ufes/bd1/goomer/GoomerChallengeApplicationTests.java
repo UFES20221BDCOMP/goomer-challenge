594d4b529dfe32370e85afd9a8d78e5e4dbbf515
Aryelle Gomes Siqueira <aryelle.siqueira@compasso.com.br>
2022-06-12 11:49:14 -0300

    maven/spring-boot project created

---
+++ src/test/java/br/com/ufes/bd1/goomer/GoomerChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ufes.bd1.goomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoomerChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
