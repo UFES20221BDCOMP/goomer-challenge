4d20cfe80f2262194e8e91d7906b0db1c1a27189
mariadmn <mariajdmn@gmail.com>
2022-06-27 20:50:35 -0300

    feat: implementação do ProductController e do RestaurantController

--- src/main/java/br/com/ufes/bd1/goomer/controller/ProductController.java
+++ src/main/java/br/com/ufes/bd1/goomer/controller/ProductController.java
@@ -1,29 +1,66 @@
 package br.com.ufes.bd1.goomer.controller;
 
+import br.com.ufes.bd1.goomer.service.ProductService;
+import br.com.ufes.bd1.goomer.dto.ProductDto;
+import br.com.ufes.bd1.goomer.model.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value ="/product", produces = "application/json")
-public class ProductController {
+public class ProductController extends AbstractController{
 
+    private final ProductService productService;
 
-    @GetMapping("/{id}")
-    public String getProduct(@PathVariable Integer id) {
-        return id.toString();
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> doGet(@RequestParam Optional<Integer> id) {
+        if (id.isPresent()) {
+            Product product = productService.getById(id.get());
+            return ResponseEntity.ok(ProductDto.fromEntity(product));
+        }
+        else {
+            Collection<Product> products = productService.getAll();
+            List<ProductDto> productDtos = products.stream().map(ProductDto::fromEntity).collect(Collectors.toList());
+
+            return ResponseEntity.ok(productDtos);
+        }
     }
 
     @PostMapping
-    public String registerProduct(@RequestBody String body) {
-        return body;
+    public ResponseEntity<String> doPost(@Valid @RequestBody ProductDto productDto) throws URISyntaxException {
+        Product product = productDto.toEntity();
+        productService.save(product);
+
+        return ResponseEntity.created(new URI("http://localhost:8080/product/" + product.getId())).build();
     }
 
     @PutMapping
-    public String updateProduct(@RequestBody String body) {
-        return body;
+    public ResponseEntity<String> doPut(@Valid @RequestBody ProductDto productDto) throws URISyntaxException {
+        Product product = productDto.toEntity();
+        productService.update(product);
+
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/{id}")
-    public String deleteProduct(@PathVariable Integer id) {
-        return id.toString();
+    @DeleteMapping
+    public ResponseEntity<String> doDelete(@RequestParam Integer id) {
+        productService.deleteById(id);
+
+        return ResponseEntity.ok().build();
     }
+
+
 }
--- src/main/java/br/com/ufes/bd1/goomer/controller/RestaurantController.java
+++ src/main/java/br/com/ufes/bd1/goomer/controller/RestaurantController.java
@@ -1,36 +1,68 @@
 package br.com.ufes.bd1.goomer.controller;
 
+import br.com.ufes.bd1.goomer.dto.RestaurantDto;
+import br.com.ufes.bd1.goomer.model.Restaurant;
+import br.com.ufes.bd1.goomer.service.RestaurantService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value ="/restaurant", produces = "application/json")
-public class RestaurantController {
+public class RestaurantController extends AbstractController {
+    private final RestaurantService restaurantService;
+
+    @Autowired
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
 
     @GetMapping
-    public String getRestaurants() {
-        return "restaurants";
+    public ResponseEntity<Object> doGet(@Valid @RequestParam Optional<Integer> id) {
+        if (id.isPresent()) {
+            Restaurant restaurant = restaurantService.getById(id.get());
+
+            return ResponseEntity.ok(
+                    RestaurantDto.fromEntity(restaurant));
+        }
+        else {
+            List<RestaurantDto> restaurants = restaurantService.getAll()
+                    .stream().map(RestaurantDto::fromEntity)
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(restaurants);
+        }
     }
 
-    @GetMapping("/{id}")
-    public String getRestaurant(@PathVariable Integer id) {
-        return id.toString();
-    }
+    @PostMapping("/restaurant")
+    public ResponseEntity<String> doPostRestaurant(@Valid @RequestBody RestaurantDto restaurantDto) throws URISyntaxException {
+        Restaurant restaurant = restaurantDto.toEntity();
+        restaurantService.save(restaurant);
 
-    @PostMapping
-    public String registerRestaurant(@RequestBody String body) {
-        return body;
+        return ResponseEntity.created(new URI("http://localhost:8080?id=" + restaurant.getId())).build();
     }
 
     @PutMapping
-    public String updateRestaurant(@RequestBody String body) {
-        return body;
+    public ResponseEntity<String> doPut(@Valid @RequestBody RestaurantDto restaurantDto) {
+        Restaurant restaurant = restaurantDto.toEntity();
+        restaurantService.update(restaurant);
+
+        return ResponseEntity.ok("Updated");
     }
 
     @DeleteMapping("/{id}")
-    public String deleteRestaurant(@PathVariable Integer id) {
-        return id.toString();
+    public ResponseEntity<String> doDelete(@PathVariable int id) {
+        restaurantService.deleteById(id);
+
+        return ResponseEntity.ok("Deleted");
     }
 
     @GetMapping("/{id}/inventory")
