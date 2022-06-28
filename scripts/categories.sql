c60164ddd5ac685e1246eaef018e3390121c82bb
Aryelle Gomes Siqueira <61244675+AryelleSiqueira@users.noreply.github.com>
2022-06-28 02:48:22 -0300

    Feature/readme and docker (#4)
    * feat: readme and docker-compose file

---
+++ scripts/categories.sql
@@ -0,0 +1,9 @@
+insert into product_category (id, name) values (1, 'BEBIDAS');
+insert into product_category (id, name) values (2, 'ENTRADAS');
+insert into product_category (id, name) values (3, 'SALADAS');
+insert into product_category (id, name) values (4, 'SOBREMESAS');
+insert into product_category (id, name) values (5, 'PRATOS_PRINCIPAIS');
+insert into product_category (id, name) values (6, 'MOLHOS');
+insert into product_category (id, name) values (7, 'PRATOS_FRIOS');
+insert into product_category (id, name) values (8, 'MASSAS');
+insert into product_category (id, name) values (9, 'APERITIVOS');
--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into product_category (id, name) values (1, 'BEBIDAS') on conflict do nothing;
-insert into product_category (id, name) values (2, 'ENTRADAS') on conflict do nothing;
-insert into product_category (id, name) values (3, 'SALADAS') on conflict do nothing;
-insert into product_category (id, name) values (4, 'SOBREMESAS') on conflict do nothing;
-insert into product_category (id, name) values (5, 'PRATOS_PRINCIPAIS') on conflict do nothing;
-insert into product_category (id, name) values (6, 'MOLHOS') on conflict do nothing;
-insert into product_category (id, name) values (7, 'PRATOS_FRIOS') on conflict do nothing;
-insert into product_category (id, name) values (8, 'MASSAS') on conflict do nothing;
-insert into product_category (id, name) values (9, 'APERITIVOS') on conflict do nothing;
